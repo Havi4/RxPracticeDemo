a6d6d3ba8795b3f71c594a2980f19f2b43c23c96
Havi.X.Li.-ND@disney.com <>
2018-07-25 09:15:16 +0800

    new

--- DayOne/DayOneController.swift
+++ DayOne/DayOneController.swift
@@ -8,8 +8,32 @@
 
 import Foundation
 import UIKit
+import RxCocoa
+import RxSwift
 
 class DayOneViewController: UIViewController {
+    
+    lazy var tapButton: UIButton = {
+        let button = UIButton.init(type: UIButtonType.custom)
+        button.frame = CGRect.init(x: 50, y: 100, width: 200, height: 44)
+        button.setTitle("tapedMe", for: UIControlState.normal)
+        button.setTitleColor(UIColor.red, for: UIControlState.normal)
+        return button
+    }()
+    
+    lazy var resetButton: UIButton = {
+        let button = UIButton.init(type: UIButtonType.custom)
+        button.frame = CGRect.init(x: 50, y: 155, width: 200, height: 44)
+        button.setTitle("reset", for: UIControlState.normal)
+        button.setTitleColor(UIColor.red, for: UIControlState.normal)
+        return button
+    }()
+    
+    lazy var numberLabel: UILabel = {
+        let label = UILabel()
+        label.frame = CGRect.init(x: 50, y: 205, width: 200, height: 44)
+        return label
+    }()
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
@@ -21,6 +45,42 @@ class DayOneViewController: UIViewController {
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        view.backgroundColor = UIColor.white
+        view.addSubview(tapButton)
+        view.addSubview(resetButton)
+        view.addSubview(numberLabel)
+        bindRxData()
+    }
+    
+    func bindRxData() {
+        _ = tapButton.rx.tap.subscribe { [weak self] (event) in
+            guard let strongSelf = self else {
+                return
+            }
+            
+            guard let text = strongSelf.numberLabel.text else {
+                return
+            }
+            guard let number = Int(text) else {
+                return
+            }
+            
+            strongSelf.numberLabel.text = String(number+1)
+        }
+        
+        _ = resetButton.rx.tap.subscribe({ [weak self] (event) in
+            guard let strongSelf = self else {
+                return
+            }
+            strongSelf.numberLabel.text = "0"
+        })
+        
+//        _ = Observable.create({ (<#AnyObserver<_>#>) -> Disposable in
+//            <#code#>
+//        })
     }
     
 }
